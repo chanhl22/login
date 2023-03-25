295f29251a653a938cb268c9db870e64b90c094a
chanhl22 <lch9502@naver.com>
2023-03-26 08:42:21 +0900

    서블릿 필터 - 요청 로그

--- src/main/java/hello/login/web/filter/LogFilter.java
+++ src/main/java/hello/login/web/filter/LogFilter.java
@@ -8,7 +8,9 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.UUID;
 
 @Slf4j
 public class LogFilter implements Filter {
@@ -20,6 +22,17 @@ public class LogFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         log.info("log filter doFilter");
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String requestURI = httpRequest.getRequestURI();
+        String uuid = UUID.randomUUID().toString();
+        try {
+            log.info("REQUEST  [{}][{}]", uuid, requestURI);
+            chain.doFilter(request, response);
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            log.info("RESPONSE [{}][{}]", uuid, requestURI);
+        }
     }
 
     @Override
